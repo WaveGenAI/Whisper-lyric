02bf9529a18afadca12f7324a21b3ad71a3469cb
Jourdelune <jourdelune863@gmail.com>
2024-06-02 11:52:48 +0200

    [update] align audio by worlds level

--- dataset/aeneas_wrapper.py
+++ dataset/aeneas_wrapper.py
@@ -1,9 +1,16 @@
 import json
+import re
 import tempfile
 
-from aeneas.tools.execute_task import ExecuteTaskCLI
+from aeneas.tools.execute_task import ExecuteTaskCLI, RuntimeConfiguration
 
 from dataset.exceptions import AeneasAlignError
+
+rconf = RuntimeConfiguration()
+rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH] = True
+rconf[RuntimeConfiguration.MFCC_MASK_NONSPEECH_L3] = True
+rconf[RuntimeConfiguration.TTS_CACHE] = True
+rconf.set_granularity(3)
 
 
 def aeneas_cli_exec(audio_path: str, lyric_path: str) -> dict:
@@ -22,15 +29,29 @@ def aeneas_cli_exec(audio_path: str, lyric_path: str) -> dict:
 
     tmp_dir = tempfile.mkdtemp()
 
+    with open(lyric_path, "r", encoding="utf-8") as f:
+        lyric = f.read()
+
+    # remove all text between []
+    lyric = re.sub(r"\[.*?\]", "\n", lyric)
+
+    # remove when more than 2 new lines
+    lyric = re.sub(r"\n{1,}", "\n", lyric).strip()
+
+    lyric = lyric.replace(" ", "\n")
+
+    with open(f"{tmp_dir}/lyric.txt", "w", encoding="utf-8") as f:
+        f.write(lyric)
+
     args = [
         "dummy",
         audio_path,
-        lyric_path,
+        f"{tmp_dir}/lyric.txt",
         "task_language=en|is_text_type=plain|os_task_file_format=json",
         f"{tmp_dir}/lyric.json",
     ]
 
-    exit_code = ExecuteTaskCLI(use_sys=False).run(arguments=args)
+    exit_code = ExecuteTaskCLI(use_sys=False, rconf=rconf).run(arguments=args)
 
     if exit_code != 0:
         raise AeneasAlignError("Aeneas failed to align lyrics")
--- dataset/process.py
+++ dataset/process.py
@@ -1,5 +1,7 @@
 import os
 
+from pydub import AudioSegment
+
 import dataset.exceptions
 from dataset.aeneas_wrapper import aeneas_cli_exec
 
@@ -27,18 +29,38 @@ class Process:
 
         return aeneas_cli_exec(audio_path, lyric_path)
 
-    def _split_audio(self, lyric_path: str, alignement: dict) -> list:
+    def _split_audio(
+        self, lyric_path: str, alignement: dict, split_windows: int = 32
+    ) -> list:
         """Method to split audio into 32 seconds segments with the corresponding lyrics
 
         Args:
             lyric_path (str): the path to the lyric file
             alignement (dict): the alignment data
+            split_windows (int, optional): the size of the split window in seconds. Defaults to 32.
 
         Returns:
-            list: a list that contain lyrics split into 32 seconds segments
+            list: a list of list that contain lyrics split into 32 seconds segments
         """
 
-        raise NotImplementedError
+        lyric = open(lyric_path, "r", encoding="utf-8").read()
+
+        segments = []
+        start_idx = 0
+        end_idx = 0
+
+        for fragment in alignement["fragments"]:
+            print(fragment)
+            end_idx = lyric.find(fragment["lines"][0], start_idx)
+            windows = (len(segments) + 1) * split_windows
+
+            if float(fragment["begin"]) > windows:
+                segments.append(lyric[start_idx:end_idx])
+                start_idx = end_idx
+
+        segments.append(lyric[start_idx:])
+
+        print(segments, len(segments))
 
     def process(self) -> None:
         """Method to process the dataset :
@@ -56,3 +78,7 @@ class Process:
             except dataset.exceptions.AeneasAlignError as e:
                 print(f"Failed to align {audio_f}: {e}")
                 continue
+
+            self._split_audio(lyric_path, alignement)
+
+            break
